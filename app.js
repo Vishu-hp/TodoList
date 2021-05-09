aca5457269fb5b248cb1abb0ae1c85c76c8992c5
DOM-Vishu <70435386+Vishu-hp@users.noreply.github.com>
2021-05-09 13:22:33 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,13 +1,12 @@
-//Selectors
 const todoInput = document.querySelector('.todo-input');
 const todoButton = document.querySelector('.todo-button');
 const todoList = document.querySelector('.todo-list');
 
-//Event Listeners
+
 todoButton.addEventListener('click',addTodo);
 todoList.addEventListener('click',deleteCheck);
 
-//Functions
+
 
 function addTodo(event){
     //Prevent form from submitting
